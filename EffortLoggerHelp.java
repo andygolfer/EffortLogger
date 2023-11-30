37fc82c305878bad3a3267526c5ab7ef701d0f91
Hoang Nguyen <99687696+torahoang@users.noreply.github.com>
2023-11-29 18:48:53 -0700

    Add files via upload
    updated

--- EffortLoggerHelp.java
+++ EffortLoggerHelp.java
@@ -1,13 +1,21 @@
 package EffortLogger;
 
+//File Name: EffortLoggerHelp.java
+//Creator: Quy Hoang Nguyen
+//Last Updated: 11/29/2023
+//Project: CSE360 Team Th15 EFFORTLOGGER V2
+//Description: 
+//
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+@SuppressWarnings("serial")
 public class EffortLoggerHelp extends JFrame {
 
     public EffortLoggerHelp() {
@@ -16,39 +24,36 @@ public class EffortLoggerHelp extends JFrame {
         setSize(300, 300);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null); // Center the frame
+
+        // Create components
+        JLabel infoLabel = new JLabel("<html>Grader Login:<br/>Username: user<br/>Password: password</html>");
+        JButton tutorialButton = new JButton("Tutorial Videos");
+
+        // Set the action for the tutorial button
+        tutorialButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Open the web browser and navigate to the specified website
+                openWebPage("https://drive.google.com/drive/folders/13hQGDQ12JJJc_3w4qyRORS-VJb4d0nsi?usp=sharing");
+            }
+        });
+
+        // Layout the components
+        setLayout(new BorderLayout());
+        add(infoLabel, BorderLayout.CENTER);
+        add(tutorialButton, BorderLayout.SOUTH);
     }
 
-    public void GifExample(int state) {
-        // Construct the file path for the GIF based on the state
-        String fileName = state == 0 ? "state0.gif" : "state1.gif";
-        Path gifPath = Paths.get(System.getProperty("user.dir")).resolve(fileName);
-
-        // Try to load the GIF file and create the label
-        JLabel gifLabel;
+    public void openWebPage(String url) {
+        // Try to open the URL in the default web browser
         try {
-            BufferedImage gifImage = ImageIO.read(gifPath.toFile());
-            ImageIcon gifIcon = new ImageIcon(gifImage);
-            gifLabel = new JLabel(gifIcon);
-        } catch (IOException e) {
-            // If there's an exception, log it and create a label indicating failure
+            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+            if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                desktop.browse(new URI(url));
+            }
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
-            gifLabel = new JLabel("Failed to load GIF image.");
+            JOptionPane.showMessageDialog(this, "Failed to open the web page.", "Error", JOptionPane.ERROR_MESSAGE);
         }
-
-        // Create a return button
-        JButton returnButton = new JButton("Return");
-        returnButton.addActionListener(e -> dispose()); // Close the current window
-
-        // Create a panel and add the label and button to it
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(gifLabel, BorderLayout.CENTER);
-        panel.add(returnButton, BorderLayout.SOUTH);
-
-        // Add the panel to the frame
-        setContentPane(panel);
-
-        // Show the frame
-        pack();
-        setVisible(true);
     }
 }
